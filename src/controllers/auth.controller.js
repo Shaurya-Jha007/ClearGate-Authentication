99ea33fc157e7973069d013a1cc8778899a003ab
Shaurya Jha <jhashaurya222@gmail.com>
2026-05-06 22:11:44 +0530

    Completed user identification

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,37 +4,72 @@ import jwt from "jsonwebtoken";
 import config from "../config/config.js";
 
 export async function register(req, res) {
-  const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-  const isAlreadyRegistered = await userModel.findOne({
-    $or: [{ email }, { username }],
-  });
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: `All fields are required!`,
+      });
+    }
 
-  if (isAlreadyRegistered) {
-    res.status(409).json({
-      message: "Username or email already exists.",
+    const isAlreadyRegistered = await userModel.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (isAlreadyRegistered) {
+      return res.status(409).json({
+        message: "Username or email already exists.",
+      });
+    }
+
+    const hashedPassword = createHash("sha256").update(password).digest("hex");
+
+    const user = await userModel.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      config.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    return res.status(201).json({
+      message: "User registered successfully!",
+      user: { username: user.username, email: user.email },
+      token,
+    });
+  } catch (error) {
+    console.error("Registration error:", error);
+    return res.status(500).json({
+      message: "Internal server error.",
+    });
+  }
+}
+
+export async function getMe(req, res) {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Token not found!",
     });
   }
 
-  const hashedPassword = createHash("sha256").update(password).digest("hex");
+  const decoded = jwt.verify(token, config.JWT_SECRET);
 
-  const user = await userModel.create({
-    username,
-    email,
-    password: hashedPassword,
-  });
+  const user = await userModel.findById(decoded.id);
 
-  const token = jwt.sign(
-    {
-      id: user._id,
+  return res.status(200).json({
+    message: "User fetched successfully!",
+    user: {
+      username: user.username,
+      email: user.email,
     },
-    config.JWT_SECRET,
-    { expiresIn: "1d" },
-  );
-
-  res.status(201).json({
-    message: "User registered successfully!",
-    user: { username: user.username, email: user.email },
-    token,
   });
 }
